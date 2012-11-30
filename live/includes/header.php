fda28cd74656e6e1f0bd1119fa216ce361c4541f
Drake Parker <e.drake.p@gmail.com>
2012-11-30 14:57:52 -0500

    major stuff

---
+++ live/includes/header.php
@@ -0,0 +1,11 @@
+
+		<header>
+			<div class="wrapper">
+			<h1>On <span style="letter-spacing: -0.25em">T</span>he <span style="letter-spacing: -0.15em">P</span>ath <span style="letter-spacing: -0.15em">Y</span>oga</h1> <em>in SPRING LAKE, MI</em>
+			<div id="ribbon" class="only-d">
+				<img src="images/facebook.png" alt="facebook icon" class="socmed" />
+				<img src="images/twitter.png" alt="twitter icon" class="socmed" />
+				<img src="images/newsletter.png" alt="mail icon" class="socmed" />
+			</div>
+			</div>			
+		</header> <!-- end header -->
---
+++ live/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+		
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<meta name="viewport" content="width=device-width, target-densitydpi=160dpi, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/html5reset.css" media="all">
+	<link rel="stylesheet" href="css/responsive.css" media="all">
+	<link rel="stylesheet" href="css/style.css" media="all">
+		
+		<title>On The Path Yoga in Spring Lake, MI</title>
+	</head>
+	<body>
+		<?php include('includes/header.php'); ?>
+		<div class="content">
+			<div class="wrapper">
+			<div class="section group">
+			</div>
+			</div>
+		</div>
+		<?php include('includes/footer.php'); ?>
+	</body>
+</html>
